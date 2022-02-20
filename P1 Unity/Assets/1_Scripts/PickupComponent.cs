308945225c0459dd351ce2de92197b228dbe6536
MattCastUCM <mattcast@ucm.es>
2022-02-20 18:56:25 +0100

    Cámara, recogibles, vida y arreglos al movimiento del personaje
    - Añadido material sin fricción para que el jugador no se quede enganchado a las paredes.
    - Añadido paquete de Cinemachine para hacer la cámara.
    - Añadida cámara con seguimiento.
    - Añadido collider que limita los bordes del mapa para la cámara.
    - Añadido Script para recoger objetos (de momento solo la Energía).
    - Añadido tag "Energy" (se podría usar Duck Typing pero cada recogible tendría que tener un Script único).
    - Modificado tamaño del Prefab de Energía.
    - Añadido PlayerLifeComponent (falta registrar la vida en el GameManager).
    - Collider del terreno modificado para usar el CompositeCollider (el RigidBody del jugador colisionaba con cada casilla una por una y se podía quedar atascado).

---
+++ P1 Unity/Assets/1_Scripts/PickupComponent.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupComponent : MonoBehaviour
+{
+    #region references
+    private PlayerLifeComponent _playerLife;
+
+    private Transform _myTransform;
+    #endregion
+
+
+    #region parameters
+    private float _timer = 0f;      // Tiempo que dura el movimiento
+
+    [SerializeField]                    
+    private float _speed = 1f;      // Velocidad del movimiento
+    #endregion
+
+
+    #region methods
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _playerLife = collision.GetComponent<PlayerLifeComponent>();
+
+        if (_playerLife != null)
+        {
+            Destroy(gameObject);
+
+            if (_myTransform.tag == "Energy")
+            {
+                // Añadir contador de energía en el GameManager
+                //GameManager.Instance.AddEnergy(1);
+
+                Debug.Log("Energy picked");
+            }
+
+            // Añadir más tags para los kits de vida, setas, o sobrecargas de energía
+        }
+    }
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myTransform = transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        // Movimiento del objeto
+        _myTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        // Cambia la dirección del movimiento y resetea el temporizador
+        if(_timer > 0.7f)
+        {
+            _speed = -_speed;
+            _timer = 0f;
+        }
+    }
+}
---
+++ P1 Unity/Assets/1_Scripts/PlayerLifeComponent.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLifeComponent : MonoBehaviour
+{
+
+    #region parameters
+    [SerializeField]
+    private int _maxLife = 3;           // Vida inicial
+
+    [SerializeField]
+    private int _hitDamage = 1;         // Vida perdida al recibir daño
+
+    [SerializeField]
+    private float _gracePeriod = 3f;          // Tiempo que dura el periodo de gracia
+    private float _timer = 0f;                // Tiempo que lleva activo el periodo de gracia
+    #endregion
+
+
+    #region properties
+    [SerializeField]
+    private int _currentLife;          // Vida restante
+    #endregion
+
+
+    #region methods
+    public void Damage()
+    {
+        // Si el periodo de gracia no está activo, se activa y baja la vida del jugador
+        if (_timer <= _gracePeriod)
+        {
+            _currentLife -= _hitDamage;
+
+            Debug.Log("Damaged");
+
+            //GameManager.Instance.OnPlayerDamage(_currentLife);
+
+            // ** LA COMPROBACIÓN DE SI EL JUGADOR HA MUERTO LA REALIZA EL GAMEMANAGER **
+
+            // Activa el contador del periodo de gracia
+            _timer = _gracePeriod;
+        }
+    }
+
+
+    public void Heal()
+    {
+        _currentLife += 1;
+
+        if (_currentLife > _maxLife)
+        {
+            _currentLife = _maxLife;
+        }
+
+        Debug.Log("Healed");
+
+        //GameManager.Instance.OnPlayerDamage(_currentLife);
+    }
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Inicializa la vida restante con el valor de la vida máxima
+        _currentLife = _maxLife;
+
+
+        // Inicializa la vida inicial en el GameManager
+        //GameManager.Instance.OnPlayerDamage(_maxLife);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        _hitDamage = 1;
+
+        // Desactiva el daño que recibe el jugador y actualiza el temporizador
+        if (_timer > 0)
+        {
+            _hitDamage = 0;
+
+            _timer -= Time.deltaTime;
+            Debug.Log("Invincible");
+        }
+    }
+   
+}
