6ce6681af82baba55841c6651b699135f2765179
NanxiQin <nanxiqin@ucm.es>
2022-02-23 11:40:01 +0100

    Implemento de electricidad
     -modificacion en escena "prueba"
    -Ordenar carpetas de scripts

---
+++ P1 Unity/Assets/1_Scripts/SelfActiveController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))] //añade por defecto un componente de tipo boxCollider
+public class SelfActiveController : MonoBehaviour
+{
+    [SerializeField]
+    private float _activeTime = 5f;
+    [SerializeField]
+    private float _desactiveTime = 3f;
+    [SerializeField]
+    private float _elapsedTime=0.0f;
+
+    private bool _active = true;
+
+    void Start()
+    {
+        _active = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        _elapsedTime += 1 * Time.deltaTime; //en seg
+       
+        //activado
+        if (_active && _elapsedTime>_activeTime)
+        {
+            _elapsedTime = 0;
+            GetComponent<Renderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+            _active = false;
+
+        }
+
+        else if (!_active && _elapsedTime > _desactiveTime)
+        {
+            _elapsedTime = 0;
+            GetComponent<Renderer>().enabled = true;
+            GetComponent<Collider2D>().enabled = true;
+            _active = true;
+        }
+    }
+}
