4127243cf6cf872b23ab8e76def2b995bdcf46c5
NanxiQin <nanxiqin@ucm.es>
2022-03-02 18:57:21 +0100

    Programar texto de controles

---
+++ P1 Unity/Assets/5_DialogueSystem/Dialogue-System-master/ControlText.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ControlText : MonoBehaviour
+{
+    #region reference
+    [SerializeField]
+    private Animator animator;
+    #endregion
+
+    #region properties
+    [SerializeField]
+    private float _showingTime = 5f;
+    private float _elapsedTime = 0.0f;
+    #endregion
+
+    public void ShowControl()
+    {
+        Debug.Log("Open");
+        animator.SetBool("Control", true);
+        _elapsedTime = 0;
+        enabled = true;
+    }
+    private void EndControl()
+    {
+        animator.SetBool("Control", false);
+        enabled = false;
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _elapsedTime = 0.0f;
+        enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _elapsedTime += 1 * Time.deltaTime; //en seg
+        if (_elapsedTime > _showingTime)
+        {
+            _elapsedTime = 0;
+
+            EndControl();
+        }
+    }
+}
---
+++ P1 Unity/Assets/5_DialogueSystem/Dialogue-System-master/ControlTextDetection.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider2D))]
+public class ControlTextDetection : MonoBehaviour
+{
+    [SerializeField]
+    private ControlText _controlText; 
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerMovementManager>() != null) 
+        {
+            _controlText.ShowControl();
+        }
+        Destroy(gameObject);
+    }
+}
