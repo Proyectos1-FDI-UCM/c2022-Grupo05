dd9391a5451c9815f7c024215809c5a2a5aa5a61
likang03 <likang1205wu2003@gmail.com>
2022-03-07 11:27:23 +0100

    parpadeo de objeto

---
+++ P1 Unity/Assets/1_Scripts/Items/BlinkingAnimation.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingAnimation : MonoBehaviour
+{
+    #region references 
+    [SerializeField]
+    private SpriteRenderer _objet;
+    #endregion
+    #region parameters
+    [SerializeField]
+    private float _activetime = 5.0f;
+    private float _time;
+    [SerializeField]
+    private float _frecuencia = 0.2f;
+    private bool _activado = false;
+    private bool _set = false;
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _time += Time.deltaTime;
+        if (_time > _activetime)
+        {
+            _activado = true;
+            _time = 0;
+        }
+        if (_activado)
+        {
+            if (_time > _frecuencia)
+            {
+                _time = 0;
+                _objet.enabled = _set;
+                _set = !_set;
+            }
+        }
+        
+    }
+}
