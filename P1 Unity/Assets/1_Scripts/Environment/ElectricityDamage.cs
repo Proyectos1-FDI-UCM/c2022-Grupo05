496055fe50eb81f2246d6abcb20b435af713ba18
MattCastUCM <mattcast@ucm.es>
2022-03-18 00:04:29 +0100

    Nivel 1 terminado
    - Modificado añadido Box Collider al Prefab del jugador para que el Capsule Collider no haga que se resbale por detrás.
    - Añadido Script para que la Electricidad haga daño.
    - Modificado Prefab del Flee porque su collision se quedaba en un sitio fijo.
    - Modificado terreno del nivel 0 para que no haya tantas zonas totalmente horizontales.

---
+++ P1 Unity/Assets/1_Scripts/Environment/ElectricityDamage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricityDamage : MonoBehaviour
+{
+    #region references 
+    private PlayerLifeComponent _playerLife;
+    #endregion
+
+    #region methods
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _playerLife = collision.GetComponent<PlayerLifeComponent>();
+
+        if (_playerLife == null) _playerLife = collision.GetComponentInParent<PlayerLifeComponent>();
+        if (_playerLife != null)
+        {
+            _playerLife.Damage();
+        }
+    }
+
+    #endregion
+}
