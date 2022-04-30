7e69e31c851a46495db915e214c02d8432986231
MattCastUCM <mattcast@ucm.es>
2022-04-30 02:50:13 +0200

    Arreglado sprite intermitente, cámara nivel 1
    - Modificado Prefab del Slime para que se renderice por detrás de los pinchos.
    - Modificados límites de la cámara antes de la zoona final del nivel 1.
    - Modificado BossInAreaTrigger para que se desactiven los objetos del pasillo cuando empieza la segunda fase.

--- P1 Unity/Assets/1_Scripts/UI/BossInAreaTrigger.cs
+++ P1 Unity/Assets/1_Scripts/UI/BossInAreaTrigger.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BossInAreaTrigger : MonoBehaviour
@@ -6,15 +8,30 @@ public class BossInAreaTrigger : MonoBehaviour
     private InputManager player;
     [SerializeField] private HUDController _hudController;
     [SerializeField] private GameObject _bossObject;
+
+    [SerializeField] private GameObject[] _items;
+
     #endregion
 
     #region methods
 
     public void SpawnBoss()
     {
+        StartCoroutine(SpawnBossCoroutine());
+    }
+
+    IEnumerator SpawnBossCoroutine()
+    {
+        yield return new WaitForSeconds(1.5f);
+        foreach (GameObject _item in _items)
+        {
+            if (_item != null)
+            {
+                _item.SetActive(false);
+            }
+        }
         _bossObject.SetActive(true);
         _hudController.ShowBossBar(true);
-
     }
 
     #endregion
